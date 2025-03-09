fee04fe5218adcc1f814d19e392fb2b6ecd2e73c
NOTMASTER09 <41077257+NOTMASTER09@users.noreply.github.com>
2025-03-09 18:36:09 +0200

    Add final anywhere where possible

--- src/main/java/net/notmaster/gtexolegs/common/data/GTExoLegsCreativeTabs.java
+++ src/main/java/net/notmaster/gtexolegs/common/data/GTExoLegsCreativeTabs.java
@@ -6,7 +6,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.notmaster.gtexolegs.GTExoLegs;
 
 public class GTExoLegsCreativeTabs {
-    public static RegistryEntry<CreativeModeTab> EXOLEGS_CREATIVE_TAB = GTExoLegs.REGISTRATE.defaultCreativeTab(GTExoLegs.MOD_ID,
+    public static final RegistryEntry<CreativeModeTab> EXOLEGS_CREATIVE_TAB = GTExoLegs.REGISTRATE.defaultCreativeTab(GTExoLegs.MOD_ID,
                     builder -> builder
                             .displayItems(new GTCreativeModeTabs.RegistrateDisplayItemsGenerator(GTExoLegs.MOD_ID, GTExoLegs.REGISTRATE))
                             .title(GTExoLegs.REGISTRATE.addLang("itemGroup", GTExoLegs.id("creative_tab"), "GT Exoskeleton Legs"))
--- src/main/java/net/notmaster/gtexolegs/common/data/GTExoLegsItems.java
+++ src/main/java/net/notmaster/gtexolegs/common/data/GTExoLegsItems.java
@@ -16,34 +16,34 @@ public class GTExoLegsItems {
     }
 
     @SuppressWarnings("unused")
-    public static ItemEntry<ItemExoskeletonLegs> LV_EXOSKELETON_LEGS = REGISTRATE
+    public static final ItemEntry<ItemExoskeletonLegs> LV_EXOSKELETON_LEGS = REGISTRATE
             .item("lv_exoskeleton_legs", p -> ItemExoskeletonLegs.create(p, GTValues.LV))
             .lang("LV Exoskeleton Legs")
             .register();
 
     @SuppressWarnings("unused")
-    public static ItemEntry<ItemExoskeletonLegs> MV_EXOSKELETON_LEGS = REGISTRATE
+    public static final ItemEntry<ItemExoskeletonLegs> MV_EXOSKELETON_LEGS = REGISTRATE
             .item("mv_exoskeleton_legs", p -> ItemExoskeletonLegs.create(p, GTValues.MV))
             .lang("MV Exoskeleton Legs")
             .tag(GTExoLegs.EXOLEGS_TAG)
             .register();
 
     @SuppressWarnings("unused")
-    public static ItemEntry<ItemExoskeletonLegs> HV_EXOSKELETON_LEGS = REGISTRATE
+    public static final ItemEntry<ItemExoskeletonLegs> HV_EXOSKELETON_LEGS = REGISTRATE
             .item("hv_exoskeleton_legs", p -> ItemExoskeletonLegs.create(p, GTValues.HV))
             .lang("HV Exoskeleton Legs")
             .tag(GTExoLegs.EXOLEGS_TAG)
             .register();
 
     @SuppressWarnings("unused")
-    public static ItemEntry<ItemExoskeletonLegs> EV_EXOSKELETON_LEGS = REGISTRATE
+    public static final ItemEntry<ItemExoskeletonLegs> EV_EXOSKELETON_LEGS = REGISTRATE
             .item("ev_exoskeleton_legs", p -> ItemExoskeletonLegs.create(p, GTValues.EV))
             .lang("EV Exoskeleton Legs")
             .tag(GTExoLegs.EXOLEGS_TAG)
             .register();
 
     @SuppressWarnings("unused")
-    public static ItemEntry<ItemExoskeletonLegs> IV_EXOSKELETON_LEGS = REGISTRATE
+    public static final ItemEntry<ItemExoskeletonLegs> IV_EXOSKELETON_LEGS = REGISTRATE
             .item("iv_exoskeleton_legs", p -> ItemExoskeletonLegs.create(p, GTValues.IV))
             .lang("IV Exoskeleton Legs")
             .tag(GTExoLegs.EXOLEGS_TAG)
